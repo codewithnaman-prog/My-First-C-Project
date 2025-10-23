20fea943ba13292bd63c7faf32feea2a9795ebfc
codewithnaman-prog <itcsnaman211203@gmail.com>
2025-10-23 15:36:06 +0530

    Delete #LAB_01/intro.c

--- #LAB_01/intro.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main (){
-    printf("Naman Kumawat\n");
-    printf("Roll No. : MEB25056\n");
-    printf("Department : Mechanical\n");
-    printf("Tezpur University\n");
-    return 0;
-}
