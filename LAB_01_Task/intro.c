959ed7febe1c69df5a0eb0f6eb202291171b1fd2
codewithnaman-prog <itcsnaman211203@gmail.com>
2025-10-23 15:37:34 +0530

    added two files
    added helloworld.c and intro.c successfully

---
+++ LAB_01_Task/intro.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main (){
+    printf("Naman Kumawat\n");
+    printf("Roll No. : MEB25056\n");
+    printf("Department : Mechanical Engineering\n");
+    printf("Tezpur University\n");
+    return 0;
+}
